ef78ee7d6c6350bd9ae70cc5e88d70560181ce1f
dd-duszynski <dd.duszynski@gmail.com>
2025-02-17 23:24:14 +0100

    feat: initial dogs page

--- app/dogs/page.tsx
+++ app/dogs/page.tsx
@@ -1,3 +1,22 @@
+import { DogCard } from '@/components/dog-card/dog-card';
+
 export default function DogsPage() {
-  return <div>Dogs</div>;
+  return (
+    <div className='flex gap-3 p-3'>
+      <DogCard
+        name='Kira'
+        breed='Mongrel'
+        age={3}
+        weight={13}
+        image='/kira.jpg'
+      />
+      <DogCard
+        name='Diego'
+        breed='Mongrel'
+        age={1.5}
+        weight={19}
+        image='/diego.jpg'
+      />
+    </div>
+  );
 }
---
+++ components/dog-card/dog-card.tsx
@@ -0,0 +1,40 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Button } from '../ui/button';
+import Image from 'next/image';
+import Link from 'next/link';
+
+type TDogCardProps = {
+  name: string;
+  breed: string;
+  age: number;
+  weight: number;
+  image: string;
+};
+
+export function DogCard({ age, weight, name, breed, image }: TDogCardProps) {
+  return (
+    <Card className='w-[350px] flex flex-col justify-between'>
+      <CardHeader>
+        <CardTitle>{name}</CardTitle>
+        <CardDescription>Breed: {breed}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Image src={image} alt={name} width={300} height={300} />
+        <p>Age: {age}</p>
+        <p>weight: {weight}</p>
+      </CardContent>
+      <CardFooter>
+        <Link className='w-full' href={`/dogs/${name}`}>
+          <Button className='w-full'>More info</Button>
+        </Link>
+      </CardFooter>
+    </Card>
+  );
+}
