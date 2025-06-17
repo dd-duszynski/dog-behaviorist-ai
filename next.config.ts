aabd3a8225875da1527476256a1ad4f91bb7cbd9
dd-duszynski <dd.duszynski@gmail.com>
2025-06-17 12:30:57 +0200

    feat: updating next.js to canary version

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,19 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  experimental: {
+    dynamicIO: true,
   },
 };
 
